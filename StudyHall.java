e066f8f3aba3002d6e45bf2b57d25fa3cd8542da
Chanpreet-1 <37393857+Chanpreet-1@users.noreply.github.com>
2018-03-30 20:26:54 +0530

    Add files via upload

--- StudyHall.java
+++ StudyHall.java
@@ -1,25 +1,26 @@
-package studyhall;
+package w12d5;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class StudyHall {
 
-	public static int data [] = {20,40,70,80,110,150,200,210,250};
-	public static int counted (int b [], int h ) {
-		int ndx = 0;
-		while (ndx < b.length && b[ndx] < h) ndx++;
-		if (ndx >= b.length) return -1;
-		if (h == b [ndx]) return ndx;
-		else return -1;
-
-	}
 	public static void main (String [] args) {
-		int goal = 200; 
-int result = counted( data, goal);
-if(result <0)
-	System.out.println ("The search could not find: " + goal);
-else
-	System.out.println("CHANPREET SINGH");
-	System.out.println("The goal of " + goal + " was found at index: " + result);
-
+		
+		List<Double> chan = new ArrayList<Double>();
+		chan.add(0.3);
+		chan.add(0.6);
+		chan.add(0.9);
+		chan.add(1.2);
+		chan.add(1.5);
+	chan.add(1.8);
+		chan.add(2.1);
+		
+		Collections.sort(chan);
+		System.out.println("The sorted list is as follows:" + chan);
+		
 	}
-
+	
 }
